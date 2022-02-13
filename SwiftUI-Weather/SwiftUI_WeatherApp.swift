39d4f299d01436f782100ceff3856ea20a6e616b
Hiroki Kondo <kondo.matr02@gmail.com>
2022-02-13 16:26:43 +0900

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by 近藤宏輝 on 2022/02/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
